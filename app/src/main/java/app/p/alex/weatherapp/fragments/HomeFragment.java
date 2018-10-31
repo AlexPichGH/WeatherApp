b339e4fb4a3d8621457fa85ebe3e2163b5736307
AlexPichGH <luctator310@gmail.com>
2018-10-31 15:02:05 +0300

    фрагмент главной страницы

--- app/src/main/java/app/p/alex/weatherapp/fragments/HomeFragment.java
+++ app/src/main/java/app/p/alex/weatherapp/fragments/HomeFragment.java
@@ -8,11 +8,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class MainFragment extends Fragment {
+import app.p.alex.weatherapp.R;
+
+public class HomeFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        return view;
     }
 }
